a17ae32851d932abef583998e5ba5dd9ea89b49b
prem singh <premrathore654@gmail.com>
2025-01-29 10:39:26 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// echo "# githubtest" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/prem3892/githubtest.git
+// git push -u origin main
+
+
+
+
+// ? git init = to initialized git repo
+//?  git add . = to add all files and dir
+// ? git commit -m 'first change'
+//? git branch -M main
+//? git remote add origin https://github.com/prem3892/githubtest.git
+//? git push -u origin main
